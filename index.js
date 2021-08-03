e780931f95243d176b82b6767ac11347a7b57037
Redcommander <45720158+Redcommander@users.noreply.github.com>
2021-08-03 19:40:18 +0200

    created base code to connect to a server

---
+++ index.js
@@ -0,0 +1,120 @@
+/////////////// config ///////////////
+
+// game server ip & port //
+const serverIp = "127.0.0.1"
+const serverPort = 7777
+
+// user data
+const userName = "PlayerName"
+const userId = "PlayerId"
+
+/////////////// code ///////////////
+
+
+const express = require("express")
+const app = express()
+
+const http = require("http")
+
+const server = http.createServer(app)
+
+const socketio = require("socket.io")
+
+const io = socketio(server, {
+    path: "/socket.io/",
+    serveClient: false,
+    transports: ['websocket', 'polling']
+})
+
+// GET > /authenticate/service_status/
+app.get("/authenticate/service_status/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        response: {
+            is_online: true
+        }
+    })
+})
+
+// POST > /authenticate/v2/authenticate/steam
+app.post("/authenticate/v2/authenticate/steam", (req, res) => {
+    res.status(200).json({
+        success: true,
+        response: {
+            auth_token: JSON.stringify(
+                {
+                    data: {
+                        public: {
+                            local_id: userId,
+                            player_name: userName
+                        }
+                    }
+                }
+            )
+        }
+    })
+})
+
+io.on('connection', (socket) => {
+    console.log("connected!")
+
+    socket.on('authenticate', (value) => {
+        console.log("authenticate")
+
+        socket.emit('user_authenticated', {
+            highest_version_in_environment: 85812,
+            event_timestamp: 123
+        })
+    })
+
+    socket.on('find', (data) => {
+        const serverAccessString = `:${serverIp}:${serverPort}`
+
+        // taken from lawbreakers, probably doesn't match exactly with RH
+        let matchmakingData = {
+            success: true,
+            is_custom_game: false,
+
+            lobby: {
+                chat_room_name: 'all',
+                team_chat_room_name: null,
+        
+                server: null,
+        
+                match_settings: {
+                    map_name: null, 
+                    game_mode: null
+                },
+        
+                current_state: 'IN_GAME',
+                game_mode: null,
+        
+                id: 'lineartutorial',
+                match_type: 0,
+                
+                playlist_type: null
+            },
+        
+            players: {},
+        
+            match_setup_state: 0,
+            map_name: null
+        }
+
+        matchmakingData.server = serverAccessString
+        matchmakingData.lobby.server = serverAccessString
+
+        matchmakingData.players[userId] = {
+            player_name: userName
+        }
+
+        matchmakingData.match_setup_state = 2
+ 
+        socket.emit('lobby_found', matchmakingData)
+    })
+
+    socket.emit('connect')
+    socket.emit('service_enabled')
+})
+
+server.listen(80)
